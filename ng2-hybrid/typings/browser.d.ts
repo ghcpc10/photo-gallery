2a0994017d1599ff1063a9e75c70810101e3145a
Chen, Chienpin <Chienpin.Chen@priceline.com>
2016-04-16 23:29:52 -0400

    feat( typings ): update tad to typings

---
+++ ng2-hybrid/typings/browser.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="browser/ambient/angular-ui-router/index.d.ts" />
+/// <reference path="browser/ambient/angular/index.d.ts" />
+/// <reference path="browser/ambient/es6-shim/index.d.ts" />
+/// <reference path="browser/ambient/jasmine/index.d.ts" />
+/// <reference path="browser/ambient/moment/index.d.ts" />
